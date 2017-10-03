a4710886ad24332f400710eae067c8d50754afab
AAKASH HANDA <AAKASH HANDA>
2017-10-03 10:12:51 +0530

    A

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {Employee} from '../models/employee.model';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+  languages=["Nodejs","AngularJs","ReactJs"];
+  model = new Employee('','');
+
+  firstNameToUpperCase(value:string){
+    if(value.length>0)
+      this.model.firstName = value.charAt(0).toUpperCase()+value.slice(1)
+    else
+      this.model.firstName=value
+  }
+}
