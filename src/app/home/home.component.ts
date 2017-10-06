eeed80dbd57bbf40e6d61b443e4f2bd668470734
AAKASH HANDA <AAKASH HANDA>
2017-10-06 09:57:01 +0530

    A

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {Employee} from '../models/employee.model';
+import {FormPoster} from '../services/form-poster.service';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'home',
@@ -11,11 +14,20 @@ export class HomeComponent {
   model = new Employee('','',true,'male','default');
   hasCodeLangError= false;
 
+  constructor(private formposter:FormPoster){
+
+  }
+
   firstNameToUpperCase(value:string){
     if(value.length>0)
       this.model.firstName = value.charAt(0).toUpperCase()+value.slice(1)
     else
       this.model.firstName=value
+  }
+
+  submitForm(form: NgForm){
+    console.log(form.value)
+    this.formposter.postEmployeeForm(this.model);
   }
 
   validOption(event){
